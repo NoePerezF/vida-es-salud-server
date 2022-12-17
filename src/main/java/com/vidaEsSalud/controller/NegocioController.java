14361c35778b832f5f698cfda1b9fe364c91ed6d
NoePerezFab <noeperez@e-gisnet.com>
2022-12-17 14:32:04 -0600

    Se agrego el cross origin en todos los controllers

--- src/main/java/com/vidaEsSalud/controller/NegocioController.java
+++ src/main/java/com/vidaEsSalud/controller/NegocioController.java
@@ -17,8 +17,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin("*")
 public class NegocioController {
     @Autowired
     private NegocioRepository repo;
--- src/main/java/com/vidaEsSalud/controller/ProductoController.java
+++ src/main/java/com/vidaEsSalud/controller/ProductoController.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin("*")
 public class ProductoController {
     
     @Autowired
